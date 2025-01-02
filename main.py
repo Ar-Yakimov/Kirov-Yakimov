1ac3f71919463f5144a40dc50370a1318c2cb146
Arseniy <ar.yakimov@gmail.com>
2025-01-02 14:32:30 +0300

    base structure

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# pygame project
+import pygame
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            # при закрытии окна
+            if event.type == pygame.QUIT:
+                running = False
+
+        # отрисовка и изменение свойств объектов
+
+        pygame.display.flip()
+    pygame.quit()
