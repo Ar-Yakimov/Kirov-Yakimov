16bfae28c8738defb19bdcf6d593d5e89afa3880
Arseniy Yakimov <ar.yakimov@gmail.com>
2025-02-23 18:53:28 +0300

    new task

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 import random
 import sys
 
-from PyQt6 import uic
 from PyQt6.QtGui import QPainter, QColor
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
 
-class CircleWidget(QMainWindow):
+class CircleWidget(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
         self.circles = []
+        self.drawButton = QPushButton('Нарисовать окружность', self)
         self.drawButton.clicked.connect(self.draw_circle)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.drawButton)
+        self.setLayout(layout)
 
     def draw_circle(self):
         diameter = random.randint(10, 100)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
-        self.circles.append((x, y, diameter))
+
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        self.circles.append((x, y, diameter, color))
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.setBrush(QColor(255, 255, 0))  # Желтый цвет
-        for (x, y, diameter) in self.circles:
+
+        for (x, y, diameter, color) in self.circles:
+            painter.setBrush(color)
             painter.drawEllipse(x, y, diameter, diameter)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Рисование окружностей 2")
+
+        self.circleWidget = CircleWidget()
+        self.setCentralWidget(self.circleWidget)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = CircleWidget()
-    ex.show()
+    mainWin = MainWindow()
+    mainWin.resize(800, 800)
+    mainWin.show()
     sys.exit(app.exec())
